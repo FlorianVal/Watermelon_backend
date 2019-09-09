5fe1e055f9da4bc6e4caf5088b96eed785781592
florian_val <florian_val@outlook.fr>
2019-09-09 16:37:25 +0200

    retrieve one user

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt'),
-      saltRounds = 10;
+  saltRounds = 10;
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
@@ -19,11 +19,11 @@ let db = mysql.createConnection({
 app.post("/users", function(req, res) {
   console.log(req.body);
   let first_name = req.body.first_name,
-      last_name = req.body.last_name,
-      email = req.body.email,
-      password = req.body.password,
-      is_admin = 0,
-      api_key = Math.random().toString(36).substring(15);
+    last_name = req.body.last_name,
+    email = req.body.email,
+    password = req.body.password,
+    is_admin = 0,
+    api_key = Math.random().toString(36).substring(15);
   bcrypt.hash(password, saltRounds, function(err, hash) {
     let query = `INSERT INTO users (first_name, last_name, email, password, is_admin, api_key) VALUES ('${first_name}', '${last_name}', '${email}', '${hash}', '${is_admin}', '${api_key} ')`;
     db.query(query, function(err, result, fields) {
@@ -33,8 +33,17 @@ app.post("/users", function(req, res) {
   });
 });
 
-app.get("/users", function(req, res){
+app.get("/users", function(req, res) {
   let query = 'SELECT * FROM users';
+  db.query(query, function(err, result, fields) {
+    if (err) throw err;
+    res.send(JSON.stringify(result));
+  });
+});
+
+app.get("/users/:id", function(req, res) {
+  let id = req.params.id;
+  let query = `SELECT * FROM users WHERE id=${id}`;
   db.query(query, function(err, result, fields) {
     if (err) throw err;
     res.send(JSON.stringify(result));
